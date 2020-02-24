aa2d22c9621a19d8eaf2b493d9faddafc4d0e41b
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-24 22:27:29 +0100

    añadiendo lógica a botón Go!

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,4 +5,25 @@ $(document).ready(function (){
     console.log('username=John Doe; expires=' + date)
     document.cookie = 'username=John Doe; expires=' + date
     */
+
+    $('#rankingBtn').click(function (){
+        alert('ranking!')
+    })
+
+    $('#goBtn').click(function(){
+
+        // Llamar a get apikey token
+        var token = 'thisisamocktoken'
+        
+        var nickname = $('#nicknameInput').val()
+
+        var expires = new Date()
+        expires.setMinutes(expires.getMinutes() + 15)
+        document.cookie = 'nickname='+nickname+'; expires='+expires
+        document.cookie = 'token='+token+'; expires='+expires
+        console.log(document.cookie)
+
+        // Navigate to go! page
+        window.location.href = "poll.html";
+    })
 })
