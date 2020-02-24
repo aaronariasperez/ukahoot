33c08d47db743a6c0359204bb2bd1da22938e8f7
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-25 00:00:44 +0100

    creada libreria de funciones custom

---
+++ frontend/js/functions.js
@@ -0,0 +1,19 @@
+function setCookie(cookieName, cookieValue, expiresInMinutes){
+    var expires = new Date()
+    expires.setMinutes(expires.getMinutes() + expiresInMinutes)
+    document.cookie = cookieName+'='+cookieValue+'; expires='+expires
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    //alert(document.cookie);
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') { 
+            c = c.substring(1);
+        }
+        if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
