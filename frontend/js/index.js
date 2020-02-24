e5506a989b5728cafc4ae56a4a3edd7380b7fb66
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-24 22:09:18 +0100

    subida inicial del frontend

---
+++ frontend/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function (){
+    /* cookie snippet
+    var date = new Date()
+    date.setMinutes(date.getMinutes() + 5)
+    console.log('username=John Doe; expires=' + date)
+    document.cookie = 'username=John Doe; expires=' + date
+    */
+})
