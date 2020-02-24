2139fb70d6e39cd555cb5674f944418369390b33
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-25 00:40:06 +0100

    externalizando control de errores

--- frontend/js/poll.js
+++ frontend/js/poll.js
@@ -1,34 +1,28 @@
 
-
-function clearErrors(){
-    $('#errorList').html('')
-    $('#errorBox').hide()
-}
-
-function initComponents(){
-    clearErrors()
-}
-
-function displayError(error) {
-    console.log(error)
-    $('#errorList').append('<ion-item><ion-text>'+error+'</ion-text></ion-item>')
-    $('#errorBox').show()
+function initComponents(pollid){
+    $('#pollidTitle').html(pollid)
 }
 
 function assertCookies() {
     var nickname = getCookie('nickname')
+    var pollid = getCookie('pollid')
     var token = getCookie('token')
 
     if (nickname == null || nickname.length <= 0)
-        displayError('You must provide an nickname')
+        error('Invalid input', 'You must provide a nickname.')
+    if (pollid == null || pollid.length <= 0)
+        error('Invalid input', 'You must provide a valid poll identifier.')
     if (token == null || token.length <= 5)
-        displayError('Area restricted. Invalid session token.')
+        error('Forbidden access', 'Area restricted. Invalid session token.')
 }
 
 $(document).ready(function (){
-    initComponents()
+
     assertCookies()
 
+    //var pollid = getCookie('pollid')
+    var pollid = 99999
 
-
+    initComponents(pollid)
+    
 })
