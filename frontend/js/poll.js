8b93649541f1e64daa910fd3e4a32a762be2f8cd
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-25 00:04:47 +0100

    depurando display de errores y externalizado getCookie a functions.js

--- frontend/js/poll.js
+++ frontend/js/poll.js
@@ -1,26 +1,18 @@
-function getCookie(name) {
-    var nameEQ = name + "=";
-    //alert(document.cookie);
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') { 
-            c = c.substring(1);
-        }
-        if (c.indexOf(nameEQ) != -1) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+
+
+function clearErrors(){
+    $('#errorList').html('')
+    $('#errorBox').hide()
 }
 
 function initComponents(){
-    var errorList = $('#errorList')
-    errorList.html('')
-    errorList.hide()
+    clearErrors()
 }
 
 function displayError(error) {
     console.log(error)
     $('#errorList').append('<ion-item><ion-text>'+error+'</ion-text></ion-item>')
+    $('#errorBox').show()
 }
 
 function assertCookies() {
@@ -34,8 +26,9 @@ function assertCookies() {
 }
 
 $(document).ready(function (){
-
+    initComponents()
     assertCookies()
+
 
 
 })
