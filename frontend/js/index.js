50a78f6dcdb2a410c6d92a2207a76a0ae0d491f4
José Crespo Guerrero <josecrespoguerrero@gmail.com>
2020-02-25 03:48:53 +0100

    logica js a login de index

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -10,6 +10,9 @@ function clearErrors(){
     var errorList = $('#errorList')
     errorList.html('')
     errorList.hide()
+    errorList = $('#loginErrorList')
+    errorList.html('')
+    errorList.hide()
 }
 
 function initComponents(){
@@ -26,11 +29,18 @@ function displayError(error){
     errorList.show()
 }
 
+function displayLoginError(error){
+    console.log(error)
+    var errorList = $('#loginErrorList')
+    errorList.append('<ion-item><ion-text color="danger">'+error+'</ion-text></ion-item>')
+    errorList.show()
+}
+
 function validateGoInput(nickname, pollid){
     clearErrors()
     var valid = true
     if (nickname == null || nickname.length <= 0){
-        displayError('You must provide a nickname')
+        displayError('You must provide a nickname.')
         valid = false
     }
     if (pollid == null || pollid.length <= 5){
@@ -40,29 +50,53 @@ function validateGoInput(nickname, pollid){
     return valid
 }
 
+function validateLoginInput(username, password){
+    clearErrors()
+    var valid = true
+    if (username == null || username.length <= 0){
+        displayLoginError('You must provide a username.')
+        valid = false
+    }
+    if (password == null || password.length <= 6){
+        displayLoginError('You must provide a password larger than 6 characters.')
+    }
+}
+
 $(document).ready(function (){
     clearCookies()
     initComponents()
 
+    //TODO: RANKING
     $('#rankingBtn').click(function (){
         alert('ranking!')
     })
 
     $('#goBtn').click(function(){
-
-        // Llamar a get apikey token
-        var token = ''
-        
         var nickname = $('#nicknameInput').val()
         var pollid = $('#pollidInput').val()
         if (!validateGoInput(nickname, pollid)) return;
+
+        // TODO: Llamar a POST /apikey token
+        var token = ''
 
         setCookie('nickname', nickname, 15)
         setCookie('pollid', pollid, 15)
         setCookie('token', token, 15)
         console.log(document.cookie)
-
-        // Navigate to go! page
         window.location.href = "poll.html";
+    })
+
+    $('#loginBtn').click(function(){
+        var username = $('#mgrUsername').val()
+        var password = $('#mgrPassword').val()
+        if (!validateLoginInput(username, password)) return;
+
+        //TODO: Llamar a POST /auth
+        var token = ''
+
+        setCookie('username', username, 15)
+        setCookie('password', password, 15)
+        setCookie('token', token, 15)
+        window.location.href = "dashboard.html";
     })
 })
